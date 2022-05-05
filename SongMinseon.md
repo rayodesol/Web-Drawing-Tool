f43fb38fb8d813be9b08eb8355f70961c47ef006
soob2n <wwwbyn@naver.com>
2022-05-05 21:22:30 +0900

    Add: Content's Canvas

--- SongMinseon.md
+++
@@ -1,4 +0,0 @@
-SongMinseon
-test
-안녕하세요
-내용 추가
--- web-drawing-tool/src/components/Content-part.vue
+++ web-drawing-tool/src/components/Content-part.vue
@@ -1,12 +1,56 @@
 <template>
   <v-container>
-    Canvas 삽입
+    <canvas id="canvas" width="900" height="600"></canvas>
   </v-container>
 </template>
 
 <script>
 export default {
+    name: 'Content-part',
+    data() {
+      return {
+        canvas: null,
+        ctx: null,
+      }
+    },
+    mounted() {
+      this.canvas = document.getElementById('canvas')
+      this.ctx = this.canvas.getContext('2d')
+      this.ctx.lineWidth = 2
+      this.ctx.strokeStyle = 'black'
+      this.canvas.addEventListener('mousemove', this.move)
+      this.canvas.addEventListener('mousedown', this.down)
+      this.canvas.addEventListener('mouseup', this.up)
+      this.canvas.addEventListener('mouseout', this.out)
+    },
+    methods: {
+      draw(curX, curY){
+        this.ctx.beginPath(); // 경로를 시작 설정
+        this.ctx.moveTo(this.startX, this.startY);
+        this.ctx.lineTo(curX, curY);
+        this.ctx.stroke();
+      },
 
+      move(e){
+        if(!this.dragging) return;  //dragging : false => return , dragging: true => move
+        var curX = e.offsetX, curY = e.offsetY;
+        this.draw(curX, curY);
+        this.startX = curX;
+        this.startY = curY;
+      },
+
+      down(e){
+        this.startX = e.offsetX;
+        this.startY = e.offsetY;
+        this.dragging = true;
+      },
+      up(){
+        this.dragging = false;            
+      },
+      out(){
+        this.dragging = false;
+      },
+    },
 };
 </script>
 
